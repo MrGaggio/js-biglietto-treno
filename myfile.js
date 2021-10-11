49168c9523bd987ee161af0dae30367cf162fc99
MrGaggio <steruggiu@gmail.com>
2021-10-11 14:09:59 +0100

    Aggiunti gli sconti under 18 e over 65

--- myfile.js
+++ myfile.js
@@ -5,15 +5,21 @@ const userDistance = prompt ( "Quanti km vuoi percorrere?");
 
 // Calcolare il prezzo totale del viaggio considerando che il prezzo del biglietto è definito in base ai km (0,21 a km)
 
-const ticketPrice = prompt ( userDistance * 0.21);
+let ticketPrice = userDistance * 0.21;
 
-console.log(ticketPrice)
 
 //và applicato uno sconto del 20% ai minorenni
-    //và applicato uno sconto del 40% agli over 65
+//và applicato uno sconto del 40% agli over 65
 
+let ticketPriceUnder18 = userDistance * 0.186;
+let ticketPriceOver65 = userDistance * 0.126;
 
-
+if (userAge <= 18) {
+    ticketPrice = ticketPriceUnder18
+    console.log(ticketPriceUnder18);
+} else if ( userAge >= 65 ) {
+    ticketPrice = ticketPriceOver65
+    console.log(ticketPriceOver65);
+}
     
-
 // Il prezzo totale deve essere espresso con mazzimo 2 decimli
