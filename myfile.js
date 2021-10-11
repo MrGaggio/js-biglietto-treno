41ad5e163b217804057a29b0e26dfa66b6b93fc8
MrGaggio <steruggiu@gmail.com>
2021-10-11 13:39:09 +0100

    Aggiunta Const calcolo base del costo del biglietto

--- myfile.js
+++ myfile.js
@@ -1,10 +1,15 @@
 // Chiedere all'utente il numero di km che vuole percorrere e la sua età
 
-
-
+const userAge = prompt ("Quanti anni hai?");
+const userDistance = prompt ( "Quanti km vuoi percorrere?");
 
 // Calcolare il prezzo totale del viaggio considerando che il prezzo del biglietto è definito in base ai km (0,21 a km)
-    //và applicato uno sconto del 20% ai minorenni
+
+const ticketPrice = prompt ( userDistance * 0.21);
+
+console.log(ticketPrice)
+
+//và applicato uno sconto del 20% ai minorenni
     //và applicato uno sconto del 40% agli over 65
 
 
