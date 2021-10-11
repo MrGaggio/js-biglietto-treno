229077fce88ec1cc5ee4b6b79a7c2a1a82fcab06
MrGaggio <steruggiu@gmail.com>
2021-10-11 13:27:07 +0100

    Creata struttura e commenti nel file js

---
+++ myfile.js
@@ -0,0 +1,14 @@
+// Chiedere all'utente il numero di km che vuole percorrere e la sua età
+
+
+
+
+// Calcolare il prezzo totale del viaggio considerando che il prezzo del biglietto è definito in base ai km (0,21 a km)
+    //và applicato uno sconto del 20% ai minorenni
+    //và applicato uno sconto del 40% agli over 65
+
+
+
+    
+
+// Il prezzo totale deve essere espresso con mazzimo 2 decimli
